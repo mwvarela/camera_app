1ed0d22215da04e4595e0e1a0955b0906f51ec41
Marcelo Wildi Varela <marcelowv@ciasc.sc.gov.br>
2022-02-01 14:35:08 -0300

    ajustes

--- lib/src/camera/cameras_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:camera/camera.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final camerasProvider = FutureProvider<List<CameraDescription>>(
-  (ref) async {
-    return await availableCameras();
-  },
-);
